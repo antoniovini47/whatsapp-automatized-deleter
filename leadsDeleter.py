2d7d01af0b429668e82ff77ec6b467a8430075f5
Antonio Oliveira <antoniovini47@gmail.com>
2024-10-20 16:23:44 -0300

    Added: Delete alternating on 3 positions.

--- leadsDeleter.py
+++ leadsDeleter.py
@@ -2,10 +2,24 @@ import pyautogui
 
 def deleteLeads(amount):
     print('Iniciando exclusão de leads...')
+    position = 1
     for lead in range(amount):
         print(f'Excluindo lead {lead+1} de {amount} - {round((lead+1)/amount*100, 2)}%')
-        pyautogui.click(900, 840, duration=0.8)
-        pyautogui.click(1000, 680, duration=0.8)
+        if (position==1):
+            pyautogui.click(1417, 623, duration=0.6)
+            pyautogui.click(1412, 703, duration=0.4)
+            position = 2
+        elif(position==2):
+            pyautogui.click(1417, 573, duration=0.6)
+            pyautogui.click(1412, 653, duration=0.4)
+            position = 3
+        else:
+            pyautogui.click(1417, 524, duration=0.6)
+            pyautogui.click(1412, 604, duration=0.4)
+            position = 1
+        pyautogui.countdown(1)
+        pyautogui.keyDown('enter')
+        pyautogui.keyUp('enter')
 
 read = input('Quantos leads deseja excluir? ')
 deleteLeads(int(read))
