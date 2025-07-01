bcb80abfb32833a82ae4c991ec516ad32f1d0aae
Antonio Oliveira <antoniovini47@gmail.com>
2025-07-01 15:35:11 -0300

    feat: added quitting groups

---
+++ quitting-groups.py
@@ -0,0 +1,24 @@
+import pyautogui
+
+firstClickPosition = (1828, 740);
+firstClickDuration = 0.3;
+secondClickDuration = 0.5;
+
+def deleteLeads(amount):
+    pyautogui.moveTo(firstClickPosition[0], firstClickPosition[1], duration=0.5)
+    pyautogui.click()
+    print('Iniciando exclusão de leads...')
+    for lead in range(amount):
+        estimated_time = (amount - lead - 1) * (firstClickDuration + secondClickDuration + 1)
+        minutes, seconds = divmod(estimated_time, 60)
+        print(f'Excluindo lead {lead+1} de {amount} - {round((lead+1)/amount*100, 1)}% - Tempo estimado restante: {int(minutes)}m {int(seconds)}s')
+        pyautogui.click(firstClickPosition[0], firstClickPosition[1], duration=firstClickDuration)
+        pyautogui.click(firstClickPosition[0]-65, firstClickPosition[1]-40, duration=secondClickDuration)
+        pyautogui.click(firstClickPosition[0]-410, firstClickPosition[1]-55, duration=secondClickDuration)
+        pyautogui.countdown(1)
+        pyautogui.keyDown('enter')
+        pyautogui.keyUp('enter')
+
+read = input('Quantos leads deseja excluir? ')
+deleteLeads(int(read))
+
